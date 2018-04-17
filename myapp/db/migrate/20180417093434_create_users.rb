67460a9366c2bab970d704f35c8622e971d83cd2
Xtail <xtail1996@yandex.ru>
2018-04-17 13:57:53 +0300

    Подключена бд постгрес

--- myapp/db/migrate/20180417093434_create_users.rb
+++ myapp/db/migrate/20180417093434_create_users.rb
@@ -1,7 +1,9 @@
 class CreateUsers < ActiveRecord::Migration[5.1]
   def change
     create_table :users do |t|
-
+      t.string :mickname, :unique => true
+      t.string :email, :unique => true
+      t.string :password
       t.timestamps
     end
   end

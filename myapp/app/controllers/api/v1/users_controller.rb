7667e0a81b014be5174653d0ea21b280bd027ebd
Xtail <xtail1996@yandex.ru>
2018-04-17 20:29:35 +0300

    Исправление областей видимости

--- myapp/app/controllers/api/v1/users_controller.rb
+++ myapp/app/controllers/api/v1/users_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::UsersController < ApiController
 
   # для навигации по юзерам
   def index
-    @users = User.all
+    @users = ::User.all
   end
 
   # для регистрации юзера
@@ -11,10 +11,10 @@ class Api::V1::UsersController < ApiController
     email = params[:email]
     password = params[:password]
 
-    @user = User.new(:mickname => nickname, :email => email, :password => password)
+    @user = ::User.new(:mickname => nickname, :email => email, :password => password)
     @user.save
 
-    @user = User.find_by_email(email)
+    @user = ::User.find_by_email(email)
 
     token_value = SecureRandom.urlsafe_base64(nil, false)
     @user.token = Token.new(:access_value => token_value)

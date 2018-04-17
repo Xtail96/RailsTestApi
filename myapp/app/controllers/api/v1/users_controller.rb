53078c879b1459f9fcf7a2d8c38526548142b5a5
Xtail <xtail1996@yandex.ru>
2018-04-17 16:54:34 +0300

    Удалены ненужные комментарии

--- myapp/app/controllers/api/v1/users_controller.rb
+++ myapp/app/controllers/api/v1/users_controller.rb
@@ -3,14 +3,6 @@ class Api::V1::UsersController < ApiController
   # для навигации по юзерам
   def index
     @users = User.all
-    #Api::V1::User::Index::Interactor.new.call do |m|
-    #  m.success do |response|
-    #    @users = response
-    #  end
-    #  m.failure do |response|
-    #    render json: response, status: :bad_request
-    #  end
-    #end
   end
 
   # для регистрации юзера
@@ -25,9 +17,7 @@ class Api::V1::UsersController < ApiController
     @user = User.find_by_email(email)
 
     token_value = SecureRandom.urlsafe_base64(nil, false)
-    #@user.token.new(:access_value => token_value)
     @user.token = Token.new(:access_value => token_value)
-    #@token = Token.new(:access_value => token_value)
   end
 
   # для обновления юзера
